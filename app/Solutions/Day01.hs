d96e17751777a14ecb68e93e69975c1d70884842
A-Walrus <ofek.r.guy@gmail.com>
2023-12-04 09:11:20 +0200

    Day 1 - Part 1

--- app/Solutions/Day01.hs
+++ app/Solutions/Day01.hs
@@ -1,18 +1,21 @@
 module Solutions.Day01 (solution) where
 
 import Base
+import Data.Char (digitToInt, isDigit)
 import Data.List
 
 solution :: String -> IO ()
 solution = run (Day parse part1 part2)
 
-type Parsed = ()
+type Parsed = [String]
 
 parse :: String -> Parsed
-parse = undefined
+parse = lines
 
 part1 :: Parsed -> Int
-part1 = undefined
+part1 = sum . map f
+  where
+    f = (\l -> (10 * digitToInt (head l)) + digitToInt (last l)) . filter isDigit
 
 part2 :: Parsed -> Int
 part2 = undefined
