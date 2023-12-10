04bc2735b822bd0fa2c1c31bacc19f8bc9e8bd6d
A-Walrus <ofek.r.guy@gmail.com>
2023-12-10 13:52:15 +0200

    Day 5 - Part 2 - Almost works
    It works for the example, not sure why it fails for real input

--- app/Solutions/Day05.hs
+++ app/Solutions/Day05.hs
@@ -2,8 +2,9 @@ module Solutions.Day05 (solution, part1, part2, parse) where
 
 import Base
 import Data.List
-import Data.List.Split (splitWhen)
+import Data.List.Split (chunksOf, splitWhen)
 import Data.Maybe (mapMaybe)
+import Data.Ord (comparing)
 
 solution :: String -> IO ()
 solution = run (Day parse part1 part2)
@@ -30,10 +31,27 @@ part1 (seeds, maps) = minimum $ map f seeds
     f :: Int -> Int
     f seed = foldl process seed maps
 
-process :: Int -> [Mapping] -> Int
-process val mapping = case mapMaybe (\(Mapping dest start len) -> if (val - start < len) && (val - start) >= 0 then Just (dest + val - start) else Nothing) mapping of
-  [res] -> res
-  _ -> val
+    process :: Int -> [Mapping] -> Int
+    process val mapping = case mapMaybe (\(Mapping dest start len) -> if (val - start < len) && (val - start) >= 0 then Just (dest + val - start) else Nothing) mapping of
+      [res] -> res
+      _ -> val
 
 part2 :: Parsed -> Int
-part2 = undefined
+part2 (s, maps) = minimum $ map fst $ concatMap f seeds
+  where
+    seeds = map (\[a, b] -> (a, b)) $ chunksOf 2 s
+    f seed = foldl (\a b -> concatMap (process b) a) [seed] maps
+
+process :: [Mapping] -> (Int, Int) -> [(Int, Int)]
+process mappings range = foldr (concatMap . process1) [range] mappings
+
+process1 :: Mapping -> (Int, Int) -> [(Int, Int)]
+process1 (Mapping dest mStart mLen) (rStart, rLen) = if oLen > 0 then overlap : before ++ after else [(rStart, rLen)]
+  where
+    mEnd = mStart + mLen
+    rEnd = rStart + rLen
+    (oStart, oEnd) = (max rStart mStart, min rEnd mEnd)
+    oLen = oEnd - oStart
+    overlap = (oStart + dest - mStart, oLen)
+    before = [(rStart, mStart - rStart) | rStart < mStart && mStart < rEnd]
+    after = [(mEnd, rEnd - mEnd) | mEnd < rEnd && mEnd > rStart]
