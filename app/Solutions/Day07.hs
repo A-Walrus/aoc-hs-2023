d4d4915abd834ae71ae7a23b791029b703dfa1b1
A-Walrus <ofek.r.guy@gmail.com>
2023-12-10 22:19:07 +0200

    Day 7 - Part 2

--- app/Solutions/Day07.hs
+++ app/Solutions/Day07.hs
@@ -32,31 +32,44 @@ value (Card 'A') = 14
 instance Ord Card where
   compare = comparing value
 
-data Type = High | Pair | TwoPair | Three | Full | Four | Five deriving (Eq, Ord)
+data Type = High | Pair | TwoPair | Three | Full | Four | Five deriving (Eq, Ord, Enum)
 
-handType :: Hand -> Type
-handType (Hand cards) = case counts of
-  [5] -> Five
-  [1, 4] -> Four
-  [2, 3] -> Full
-  [1, 1, 3] -> Three
-  [1, 2, 2] -> TwoPair
-  [1, 1, 1, 2] -> Pair
-  _ -> High
-  where
-    counts :: [Int]
-    counts = sort $ Map.elems $ foldr (\card map -> Map.insertWith (+) card 1 map) Map.empty cards
-
-instance Ord Hand where
-  compare = comparing (\x@(Hand cards) -> (handType x, cards))
+countType [5] = Five
+countType [1, 4] = Four
+countType [2, 3] = Full
+countType [1, 1, 3] = Three
+countType [1, 2, 2] = TwoPair
+countType [1, 1, 1, 2] = Pair
+countType [1, 1, 1, 1, 1] = High
 
 parse :: String -> Parsed
 parse = map (parseLine . words) . lines
   where
     parseLine [hand, bid] = (Hand (map Card hand), read bid)
 
+solve comp = sum . zipWith (*) [1 ..] . map snd . sortBy comp
+
 part1 :: Parsed -> Int
-part1 = sum . zipWith (*) [1 ..] . map snd . sort
+part1 = solve comp
+  where
+    comp (a, _) (b, _) = comparing (\x@(Hand cards) -> (handType x, cards)) a b
+    handType :: Hand -> Type
+    handType (Hand cards) = countType counts
+      where
+        counts :: [Int]
+        counts = sort $ Map.elems $ foldr (\card map -> Map.insertWith (+) card 1 map) Map.empty cards
 
 part2 :: Parsed -> Int
-part2 = undefined
+part2 = solve comp
+  where
+    comp (a, _) (b, _) = comparing (\x@(Hand cards) -> (handType x, map value2 cards)) a b
+    value2 (Card 'J') = 1
+    value2 x = value x
+
+    handType :: Hand -> Type
+    handType (Hand cards) = countType newCounts
+      where
+        counts :: [Int]
+        counts = sort $ Map.elems $ foldr (\card map -> Map.insertWith (+) card 1 map) Map.empty (filter (/= Card 'J') cards)
+        jokerCount = length $ filter (== Card 'J') cards
+        newCounts = if null counts then [5] else init counts ++ [last counts + jokerCount]
