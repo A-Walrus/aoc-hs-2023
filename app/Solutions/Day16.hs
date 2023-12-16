7bb228d20fbd80059e2e4b769060eff4e88429ae
A-Walrus <ofek.r.guy@gmail.com>
2023-12-16 11:44:38 +0200

    Day 16 - Part 2

--- app/Solutions/Day16.hs
+++ app/Solutions/Day16.hs
@@ -8,18 +8,32 @@ import qualified Data.Set as Set
 solution :: String -> IO ()
 solution = run (Day parse part1 part2)
 
-type Parsed = Map.Map Pos Char
+type Parsed = (Map.Map Pos Char, Pos)
 
 parse :: String -> Parsed
-parse s = Map.fromList (concat $ zipWith (\l y -> zipWith (\c x -> ((x, y), c)) l [0 ..]) (lines s) [0 ..])
+parse s = (m, dims)
+  where
+    ls = lines s
+    m = Map.fromList (concat $ zipWith (\l y -> zipWith (\c x -> ((x, y), c)) l [0 ..]) ls [0 ..])
+    dims = (length (head ls), length ls)
 
 type State = Set.Set (Pos, Dir)
 
 part1 :: Parsed -> Int
-part1 grid = length energized
+part1 (m, _) = solve (0, 0) East m
+
+part2 :: Parsed -> Int
+part2 (grid, (width, height)) = maximum $ map (\(pos, dir) -> solve pos dir grid) $ concat [top, bottom, left, right]
   where
-    topLeft = (0, 0)
-    initial = Set.fromList [(topLeft, dir) | dir <- collide East (grid Map.! topLeft)]
+    top = [((x, 0), South) | x <- [0 .. width - 1]]
+    bottom = [((x, height - 1), North) | x <- [0 .. width - 1]]
+    left = [((0, y), East) | y <- [0 .. height - 1]]
+    right = [((width - 1, y), West) | y <- [0 .. height - 1]]
+
+solve :: Pos -> Dir -> Map.Map Pos Char -> Int
+solve start dir grid = length energized
+  where
+    initial = Set.fromList [(start, d) | d <- collide dir (grid Map.! start)]
 
     energized = Set.map fst $ uncurry Set.union $ last run
     run = takeWhile (not . Set.null . fst) $ iterate f (initial, Set.empty)
@@ -56,6 +70,3 @@ collide West '/' = [South]
 collide North '/' = [East]
 collide South '/' = [West]
 collide d '.' = [d]
-
-part2 :: Parsed -> Int
-part2 = undefined
