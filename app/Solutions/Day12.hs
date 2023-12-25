e87e748e1ada6a09c92abc5c697d57d928df6e1d
A-Walrus <ofek.r.guy@gmail.com>
2023-12-25 18:46:11 +0200

    Fix Day 12

--- app/Solutions/Day12.hs
+++ app/Solutions/Day12.hs
@@ -31,7 +31,7 @@ part1 = sum . map (uncurry numOptions)
     allOptions [x] size = [replicate gap White ++ replicate x Black ++ replicate (size - gap - x) White | gap <- [0 .. (size - x)]]
     allOptions l@(x : xs) size = [replicate gap White ++ replicate x Black ++ [White] ++ rest | gap <- [0 .. maxGap], rest <- allOptions xs (size - gap - x - 1)]
       where
-        maxGap = sum l + length l - 1
+        maxGap = size - (sum l + length l - 1)
 
 part2 :: Parsed -> Int
 part2 = undefined
